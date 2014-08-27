6a4db0693f63579b96bd26b4cdd68703f2786a89
mokotoy <mokotoy@Java.local>
2014-08-27 01:55:31 -0700

    Tools Update
    PLCScan
    Wago Exploit
    ModScan
    Man Page

--- setupMoki.sh
+++ setupMoki.sh
@@ -35,6 +35,9 @@ case "$1" in
         download_rules=true
         edit_conf=true
         post_install=true
+		install_plcscan=true
+		install_wago=true
+		install_modscan=true
         shift
         ;;
     --snort | --Snort )
@@ -44,14 +47,25 @@ case "$1" in
         post_install=true
         shift
         ;;
-    --db | --digitalbond )
-        # Select This Option if Snort is already installed
+    --db | --digitalbond ) # Select This Option if Snort is already installed
         pre_install=true
         download_rules=true
         edit_conf=true
         post_install=true
         shift
         ;;
+	--plcscan )
+		install_plcscan=true
+		shift
+		;;
+	--wago )
+		install_wago=true
+		shift
+		;;
+	--modscan )
+		install_modscan=true
+		shift
+		;;
     * ) break
         ;;
    esac
@@ -65,6 +79,9 @@ if $man_page ; then
         --snort | --Snort \t Installs Snort. \n
         --db | --digitalbond \t Installs Digital Bond's ICS Snort Rules. \n
         \t\t\t Select this option if Snort is already installed. \n
+		--plcsan \t\t Installs PLCScan \n
+		--wago \t\t\t Installs Wago Exploit on 3S CoDeSys Runtime software \n
+		--modscan \t\t Installs ModScan \n
         -h | --help \t\t This manual page. \n
         -v | --verbose \t\t Verbose."
 fi
@@ -123,11 +140,13 @@ if $download_rules ; then
     
     echo "# Downloading Traffic Samples from Digital Bond"
     wget http://digitalbond.com/wp-content/uploads/2011/02/Quickdraw_PCAPS_4_0.zip
-    unzip Quickdraw_PCAPS_4_0.zip
+    unzip Quickdraw_PCAPS_4_0.zip -d Quickdraw_PCAPS_4_0
     cp Quickdraw_PCAPS_4_0 /Desktop
 	
 	echo "# Downloading Script for testing Quickdraw Snort Rules"
-	wget 
+	wget --no-check-certificate https://raw.githubusercontent.com/mokotoy/moki/master/testSnortRules.sh
+	chmod +x testSnortRules.sh
+	cp testSnortRules /Desktop
 fi
 
 
@@ -200,28 +219,70 @@ include \$RULE_PATH/vulnerability_1_5.rules" >> /etc/snort/snort.conf
 fi
 
 # Configures Snort with Digital Bond's Quickdraw SCADA IDS Snort Rules using default 
+# Only adding Modbus/TCP rules due to missing variables in other rules from missing 
+# preprocessors
 if $edit_conf ; then
 
     ## Updates Snort Configuration file
-    echo -e "#################
+    echo "#################
 # SCADA Variables
 #################
-ipvar MODBUS_CLIENT $client_address
-ipvar MODBUS_SERVER $server_address
-ipvar ENIP_CLIENT $client_address
-ipvar ENIP_SERVER $server_address
-ipvar DNP3_CLIENT $client_address
-ipvar DNP3_SERVER $server_address
+ipvar MODBUS_CLIENT $HOME_NET
+ipvar MODBUS_SERVER $HOME_NET
+ipvar ENIP_CLIENT $HOME_NET
+ipvar ENIP_SERVER $HOME_NET
+ipvar DNP3_CLIENT $HOME_NET
+ipvar DNP3_SERVER $HOME_NET
 portvar DNP3_PORTS 20000
 
 ##############
 # SCADA Rules
 ##############
-include $RULE_PATH/modbus_1_2.rules
-include $RULE_PATH/dnp3_1_2.rules
-include $RULE_PATH/enip_cip_1_1.rules
-include $RULE_PATH/vulnerability_1_5.rules" >> /etc/snort/snort.conf
+include $RULE_PATH/modbus_1_2.rules" >> /etc/snort/snort.conf
+
 fi
+
+if $install_plcscan ; then
+# Installs PLCScan
+	echo "# Installing PLCScan... "
+	wget http://plcscan.googlecode.com/svn/trunk/plcscan.py \
+	http://plcscan.googlecode.com/svn/trunk/modbus.py \
+	http://plcscan.googlecode.com/svn/trunk/s7.py
+	
+	# Create a directory for the PLCScan files
+	mkdir ~/Desktop/PLCScan
+	mv {plcscan.py,modbus.py,s7.py} ~/Desktop/PLCScan
+	
+	echo "# To execute PLCScan, type: \"Python plcscan.py\""
+	
+fi
+
+if $install_wago ; then
+# Installs Digital Bond's Wago Exploit on 3S CoDeSys Runtime Software
+	echo "# Installing Wago Exploit... "
+	wget --no-check-certificate \
+	https://raw.githubusercontent.com/mokotoy/moki/master/codesys-shell.py \
+	https://raw.githubusercontent.com/mokotoy/moki/master/codesys-transfer.py \
+	https://raw.githubusercontent.com/mokotoy/moki/master/codesys.nse
+	
+	# Create a directory for the Wago Exploit files
+	mkdir ~/Desktop/WagoExploit
+	mv {codesys-shell.py,codesys-transfer.py,codesys.nse} ~/Desktop/WagoExploit
+	
+	echo "# To execute python files, type: \"Python codesys-shell.py\""
+
+fi
+
+if $install_modscan ; then
+	# Installs ModScan
+	echo "# Installing ModScan... "
+	wget http://modscan.googlecode.com/svn/trunk/modscan.py
+	chmod +x modscan.py
+	ln -s modscan.py modscan
+	mv {modscan.py,modscan} /usr/bin
+	
+fi
+
 
 # Installs custom background and cleans up tmp folder
 if $post_install ; then
@@ -253,5 +314,7 @@ if $post_install ; then
     echo "# "
     echo "# All Done, Check the .conf file and rules directory"
     echo "# "
+	echo "# Go to Desktop and execute ./testSnortRules.sh to test Snort Rules"
+	echo "# "
 fi
 
